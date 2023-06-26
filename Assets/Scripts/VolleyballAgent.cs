adb049dc9ad51188b0f974571bf0224e639e3766
Gianfranco Demarco <gianfranco@hlpy.co>
2023-06-26 17:52:07 +0200

    An initial 1v1 experiment

--- Assets/Scripts/VolleyballAgent.cs
+++ Assets/Scripts/VolleyballAgent.cs
@@ -202,35 +202,16 @@ public class VolleyballAgent : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Agent rotation (1 float)
-        sensor.AddObservation(this.transform.rotation.y);
-
-        // Vector from agent to ball (direction to ball) (3 floats)
-        Vector3 toBall = new Vector3(
-            (ballRb.transform.position.x - this.transform.position.x) * agentRot,
-            (ballRb.transform.position.y - this.transform.position.y),
-            (ballRb.transform.position.z - this.transform.position.z) * agentRot
-        );
-
-        sensor.AddObservation(toBall.normalized);
-
-        // Distance from the ball (1 float)
-        sensor.AddObservation(toBall.normalized);
-        sensor.AddObservation(toBall.magnitude);
-
-        // Ball velocity (3 floats)
+        // Ball position and velocity (6 floats)
+        sensor.AddObservation(ball.transform.position);
         sensor.AddObservation(ballRb.velocity.y);
         sensor.AddObservation(ballRb.velocity.z * agentRot);
         sensor.AddObservation(ballRb.velocity.x * agentRot);
 
-        // Add position, velocity and rotation for all agents
-        foreach (VolleyballAgent agent in envController.GetAgents())
-        {
-            Rigidbody oppositeAgentRb = agent.GetComponent<Rigidbody>();
-            sensor.AddObservation(agent.GetComponent<Rigidbody>().position);
-            sensor.AddObservation(agent.GetComponent<Rigidbody>().velocity);
-            sensor.AddObservation(agent.GetComponent<Rigidbody>().rotation);
-        }
+        // Agent position, velocity and rotation (5 floats)
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(GetComponent<Rigidbody>().velocity);
+        sensor.AddObservation(GetComponent<Rigidbody>().rotation);
     }
 
     // For human controller
--- Assets/Scripts/VolleyballEnvController.cs
+++ Assets/Scripts/VolleyballEnvController.cs
@@ -81,12 +81,12 @@ public class VolleyballEnvController : MonoBehaviour
     }
 
     public void ResolveEvent(Event triggerEvent, VolleyballAgent triggerer, VolleyballAgent other){
-        switch (triggerEvent){
-            case Event.AgentsCollision:
-                triggerer.SetReward(-0.1f);
-                other.SetReward(-0.1f);
-                break;
-        }
+        // switch (triggerEvent){
+        //     case Event.AgentsCollision:
+        //         triggerer.SetReward(-0.1f);
+        //         other.SetReward(-0.1f);
+        //         break;
+        // }
     }
 
     /// <summary>
@@ -102,28 +102,28 @@ public class VolleyballEnvController : MonoBehaviour
         {
             case Event.HitRedAgent:
             case Event.HitBlueAgent:
-                if (IsDoubleTouch())
-                {
-                    lastHitter.SetReward(-0.5f);
-                    EndAllAgentsEpisode();
-                    ResetScene();
-                }
-                else
-                {
-                    int numberOfTeamTouches = GetNumberOfTeamTouches();
-                    if (numberOfTeamTouches > 3)
-                    {
-                        // NOTE: Second to last hitter is the one who should have hit the ball in the other field, so he gets a negative reward
-                        VolleyballAgent secondToLastHitter = hitterHistory[hitterHistory.Count - 2];
-                        secondToLastHitter.SetReward(-1f);
-                        EndAllAgentsEpisode();
-                        ResetScene();
-                    }
-                    else
-                    {
-                        lastHitter.AddReward(0.1f + 0.1f * numberOfTeamTouches);
-                    }
-                }
+                // if (IsDoubleTouch())
+                // {
+                //     lastHitter.SetReward(-0.5f);
+                //     EndAllAgentsEpisode();
+                //     ResetScene();
+                // }
+                // else
+                // {
+                //     int numberOfTeamTouches = GetNumberOfTeamTouches();
+                //     if (numberOfTeamTouches > 3)
+                //     {
+                //         // NOTE: Second to last hitter is the one who should have hit the ball in the other field, so he gets a negative reward
+                //         VolleyballAgent secondToLastHitter = hitterHistory[hitterHistory.Count - 2];
+                //         secondToLastHitter.SetReward(-1f);
+                //         EndAllAgentsEpisode();
+                //         ResetScene();
+                //     }
+                //     else
+                //     {
+                //         lastHitter.AddReward(0.1f + 0.1f * numberOfTeamTouches);
+                //     }
+                // }
                 break;
             case Event.HitOutOfBounds:
                 if (lastHitter != null)
@@ -136,25 +136,27 @@ public class VolleyballEnvController : MonoBehaviour
                 break;
 
             case Event.HitBlueGoal:
+                ApplyRewardToTeam(Team.Red, -1f);
                 // Red took the goal, so it will be penalized
                 // Blue will get a reward only if it did something
-                ApplyRewardToTeam(Team.Red, -0.5f);
-                if (PlayerOfTeamHitBall(Team.Blue))
-                {
-                    ApplyRewardToTeam(Team.Blue, 0.5f);
-                }
+                // ApplyRewardToTeam(Team.Red, -0.5f);
+                // if (PlayerOfTeamHitBall(Team.Blue))
+                // {
+                //     ApplyRewardToTeam(Team.Blue, 0.5f);
+                // }
                 EndAllAgentsEpisode();
                 ResetScene();
                 break;
 
             case Event.HitRedGoal:
+                ApplyRewardToTeam(Team.Blue, -1f);
                 // Blue took the goal, so it will be penalized
                 // Red will get a reward only if it did something
-                ApplyRewardToTeam(Team.Blue, -0.5f);
-                if (PlayerOfTeamHitBall(Team.Red))
-                {
-                    ApplyRewardToTeam(Team.Red, 0.5f);
-                }
+                // ApplyRewardToTeam(Team.Blue, -0.5f);
+                // if (PlayerOfTeamHitBall(Team.Red))
+                // {
+                //     ApplyRewardToTeam(Team.Red, 0.5f);
+                // }
                 EndAllAgentsEpisode();
                 ResetScene();
                 break;
@@ -163,7 +165,7 @@ public class VolleyballEnvController : MonoBehaviour
                 // NOTE: we probably can remove the check on the team id here
                 if (lastHitter != null && lastHitter.teamId == Team.Red)
                 {
-                    lastHitter.AddReward(0.5f);
+                    lastHitter.AddReward(1);
                 }
                 break;
 
@@ -171,16 +173,16 @@ public class VolleyballEnvController : MonoBehaviour
                 // NOTE: we probably can remove the check on the team id here
                 if (lastHitter != null && lastHitter.teamId == Team.Blue)
                 {
-                    lastHitter.AddReward(0.5f);
+                    lastHitter.AddReward(1);
                 }
                 break;
             case Event.HitWall:
-                if (lastHitter != null)
-                {
-                    lastHitter.SetReward(-1f);
-                    EndAllAgentsEpisode();
-                    ResetScene();
-                }
+                // if (lastHitter != null)
+                // {
+                //     lastHitter.SetReward(-1f);
+                //     EndAllAgentsEpisode();
+                //     ResetScene();
+                // }
                 break;
         }
     }
@@ -207,11 +209,11 @@ public class VolleyballEnvController : MonoBehaviour
         if (ballSpawnSide == 1)
         {
             // 0 and 2 must be on serving position   
-            server = AgentsList[Random.Range(0, 2)];
+            server = AgentsList[Random.Range(0, AgentsList.Count/2)];
         }
         else
         {
-            server = AgentsList[Random.Range(2, 4)];
+            server = AgentsList[Random.Range(AgentsList.Count/2, AgentsList.Count)];
         }
 
         foreach (var agent in AgentsList)
