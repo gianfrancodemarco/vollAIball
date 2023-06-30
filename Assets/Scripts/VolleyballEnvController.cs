40867c3d386f53a5911276bf75199d8ed4cbf7ce
Francesco Ranieri <ranierifra99@hotmail.it>
2023-06-30 18:41:49 +0200

    merge

--- Assets/Scripts/VolleyballEnvController.cs
+++ Assets/Scripts/VolleyballEnvController.cs
@@ -125,22 +125,28 @@ public class VolleyballEnvController : MonoBehaviour
                 break;
             case Event.HitRedAgent:
             case Event.HitBlueAgent:
-                // else
-                // {
-                //     int numberOfTeamTouches = GetNumberOfTeamTouches();
-                //     if (numberOfTeamTouches > 3)
-                //     {
-                //         // NOTE: Second to last hitter is the one who should have hit the ball in the other field, so he gets a negative reward
-                //         VolleyballAgent secondToLastHitter = hitterHistory[hitterHistory.Count - 2];
-                //         secondToLastHitter.SetReward(-1f);
-                //         EndAllAgentsEpisode();
-                //         ResetScene();
-                //     }
-                //     else
-                //     {
-                //         lastHitter.AddReward(0.1f + 0.1f * numberOfTeamTouches);
-                //     }
-                // }
+                if (IsDoubleTouch())
+                {
+                    lastHitter.SetReward(-1f);
+                    EndAllAgentsEpisode();
+                    ResetScene();
+                }
+                else
+                {
+                    int numberOfTeamTouches = GetNumberOfTeamTouches();
+                    if (numberOfTeamTouches > 3)
+                    {
+                        // NOTE: Second to last hitter is the one who should have hit the ball in the other field, so he gets a negative reward
+                        VolleyballAgent secondToLastHitter = hitterHistory[hitterHistory.Count - 2];
+                        secondToLastHitter.SetReward(-1f);
+                        EndAllAgentsEpisode();
+                        ResetScene();
+                    }
+                    // else
+                    // {
+                    //     lastHitter.AddReward(0.1f + 0.1f * numberOfTeamTouches);
+                    // }
+                }
                 break;
             case Event.HitOutOfBounds:
                 if (lastHitter != null)
